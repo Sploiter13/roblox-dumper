072b16440d345aaed9891b26c08b5151033b0319
nopjo <email@jonah.cool>
2025-12-17 19:48:51 +0000

    docs: update offsets.hpp with version number and use wildcard for _headers file

--- docs/public/offsets.hpp
+++ docs/public/offsets.hpp
@@ -1,7 +1,8 @@
 //================================================================
 // Dumped By Jonah's Roblox Dumper | Discord: jonahw
 // Github Link: https://github.com/nopjo/roblox-dumper
-// Dumped at: 2025-12-17 18:55:08
+// Dumped at: 2025-12-17 19:43:23
+// Roblox Version: version-eaebe135b3ec446b
 // Total offsets dumped: 112
 //================================================================
 
@@ -16,7 +17,7 @@ namespace offsets {
         inline constexpr uintptr_t ToFakeDataModel = 0x700;
         inline constexpr uintptr_t ViewMatrix = 0x180;
         inline constexpr uintptr_t WindowDimensions = 0x720;
-    } // namespace VisualEngine
+    }
 
     namespace RenderView {
         inline constexpr uintptr_t InvalidateLighting = 0x148;
@@ -30,25 +31,25 @@ namespace offsets {
         inline constexpr uintptr_t RunService = 0x398;
         inline constexpr uintptr_t UserInputService = 0x3A8;
         inline constexpr uintptr_t Workspace = 0x178;
-    } // namespace DataModel
+    }
 
     namespace Instance {
         inline constexpr uintptr_t AttributeContainer = 0x48;
-        inline constexpr uintptr_t AttributeList = 0x10;
+        inline constexpr uintptr_t AttributeList = 0x18;
         inline constexpr uintptr_t AttributeToNext = 0x58;
-        inline constexpr uintptr_t AttributeToValue = 0xD8;
+        inline constexpr uintptr_t AttributeToValue = 0x18;
         inline constexpr uintptr_t ChildrenEnd = 0x8;
         inline constexpr uintptr_t ChildrenStart = 0x70;
         inline constexpr uintptr_t ClassDescriptor = 0x18;
         inline constexpr uintptr_t ClassName = 0x8;
         inline constexpr uintptr_t Name = 0xB0;
         inline constexpr uintptr_t Parent = 0x68;
-    } // namespace Instance
+    }
 
     namespace Workspace {
         inline constexpr uintptr_t Camera = 0x450;
         inline constexpr uintptr_t Gravity = 0x9B0;
-    } // namespace Workspace
+    }
 
     namespace Players {
         inline constexpr uintptr_t LocalPlayer = 0x130;
@@ -59,7 +60,7 @@ namespace offsets {
         inline constexpr uintptr_t ModelInstance = 0x360;
         inline constexpr uintptr_t Team = 0x270;
         inline constexpr uintptr_t UserId = 0x298;
-    } // namespace Player
+    }
 
     namespace Team {
         inline constexpr uintptr_t Color = 0xD0;
@@ -76,13 +77,13 @@ namespace offsets {
         inline constexpr uintptr_t Shape = 0x1B1;
         inline constexpr uintptr_t Size = 0x1B0;
         inline constexpr uintptr_t Transparency = 0xF0;
-    } // namespace Part
+    }
 
     namespace PrimitiveFlags {
         inline constexpr uintptr_t Anchored = 0x2;
         inline constexpr uintptr_t CanCollide = 0x8;
         inline constexpr uintptr_t CanTouch = 0x10;
-    } // namespace PrimitiveFlags
+    }
 
     namespace Humanoid {
         inline constexpr uintptr_t Health = 0x194;
@@ -97,12 +98,12 @@ namespace offsets {
         inline constexpr uintptr_t WalkSpeed = 0x1D4;
         inline constexpr uintptr_t WalkSpeedCheck = 0x3C0;
         inline constexpr uintptr_t WalkToPoint = 0x17C;
-    } // namespace Humanoid
+    }
 
     namespace Mesh {
         inline constexpr uintptr_t MeshId = 0x2E0;
         inline constexpr uintptr_t TextureId = 0x310;
-    } // namespace Mesh
+    }
 
     namespace Value {
         inline constexpr uintptr_t Value = 0xD0;
@@ -111,7 +112,7 @@ namespace offsets {
     namespace SpecialMesh {
         inline constexpr uintptr_t MeshId = 0x108;
         inline constexpr uintptr_t Scale = 0xDC;
-    } // namespace SpecialMesh
+    }
 
     namespace Lighting {
         inline constexpr uintptr_t Ambient = 0xD8;
@@ -124,7 +125,7 @@ namespace offsets {
         inline constexpr uintptr_t EnvironmentSpecularScale = 0x128;
         inline constexpr uintptr_t OutdoorAmbient = 0x108;
         inline constexpr uintptr_t Sky = 0x1D8;
-    } // namespace Lighting
+    }
 
     namespace Sky {
         inline constexpr uintptr_t MoonAngularSize = 0x25C;
@@ -139,7 +140,7 @@ namespace offsets {
         inline constexpr uintptr_t StarCount = 0x260;
         inline constexpr uintptr_t SunAngularSize = 0x264;
         inline constexpr uintptr_t SunTextureId = 0x230;
-    } // namespace Sky
+    }
 
     namespace Atmosphere {
         inline constexpr uintptr_t Color = 0xD0;
@@ -148,48 +149,48 @@ namespace offsets {
         inline constexpr uintptr_t Glare = 0xEC;
         inline constexpr uintptr_t Haze = 0xF0;
         inline constexpr uintptr_t Offset = 0xF4;
-    } // namespace Atmosphere
+    }
 
     namespace BloomEffect {
         inline constexpr uintptr_t Intensity = 0xD0;
         inline constexpr uintptr_t Size = 0xD4;
         inline constexpr uintptr_t Threshold = 0xD8;
-    } // namespace BloomEffect
+    }
 
     namespace SunRaysEffect {
         inline constexpr uintptr_t Intensity = 0xD0;
         inline constexpr uintptr_t Spread = 0xD0;
-    } // namespace SunRaysEffect
+    }
 
     namespace ColorCorrectionEffect {
         inline constexpr uintptr_t Brightness = 0xDC;
         inline constexpr uintptr_t Contrast = 0xE0;
         inline constexpr uintptr_t Saturation = 0xE4;
         inline constexpr uintptr_t TintColor = 0xD0;
-    } // namespace ColorCorrectionEffect
+    }
 
     namespace DepthOfFieldEffect {
         inline constexpr uintptr_t Density = 0xD0;
         inline constexpr uintptr_t FocusDistance = 0xD4;
         inline constexpr uintptr_t InFocusRadius = 0xD8;
         inline constexpr uintptr_t NearIntensity = 0xDC;
-    } // namespace DepthOfFieldEffect
+    }
 
     namespace MouseService {
         inline constexpr uintptr_t InputObject = 0x100;
         inline constexpr uintptr_t Position = 0xEC;
-    } // namespace MouseService
+    }
 
     namespace UIGradient {
         inline constexpr uintptr_t Color = 0x134;
         inline constexpr uintptr_t Offset = 0x158;
         inline constexpr uintptr_t Rotation = 0x160;
         inline constexpr uintptr_t Transparency = 0xF4;
-    } // namespace UIGradient
+    }
 
     namespace Camera {
         inline constexpr uintptr_t FieldOfView = 0x160;
         inline constexpr uintptr_t Position = 0x11C;
         inline constexpr uintptr_t Rotation = 0xF8;
-    } // namespace Camera
-} // namespace offsets
+    }
+}
